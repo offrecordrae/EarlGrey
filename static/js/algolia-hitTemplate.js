8d4d3fb1a0d867539670e5e5850bd848d633e7ff
offrecordrae <raeoffrecord@gmail.com>
2021-01-27 00:15:12 +0800

    新增jQuery生成搜索結果之html結構

---
+++ static/js/algolia-hitTemplate.js
@@ -0,0 +1,6 @@
+var hitTemplate =
+    '<a href="{{url}}" class="hit-title">' +
+    '<h2>{{{_highlightResult.title.value}}}</h2></a>' +
+    '<span class="post-meta">{{category}} | </span>' +
+    '<span class="post-meta">{{tag}}</span>' +
+    '<p>{{{_snippetResult.content.value}}}</p>';
